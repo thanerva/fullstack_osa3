bce5a0ab640f987a51c5080baa970f0d17cfa9f7
thanerva <bgj170@myy.haaga-helia.fi>
2021-07-15 18:50:30 +0300

    mongoDB and mongoose

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as a argument pls");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const name = process.argv[3];
+
+const number = process.argv[4];
+
+const url = `mongodb+srv://user1:${password}@cluster0.kqkzi.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: `${name}`,
+  number: `${number}`,
+});
+if (process.argv.length > 3) {
+  person.save().then((response) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+}
